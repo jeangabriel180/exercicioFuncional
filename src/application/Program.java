79ea0ea13d2fa93114b5cf680fb84eb2e882f08d
Jean Gabriel <jeangabriel180@hotmail.com>
2022-03-20 18:42:57 -0300

    criando projeto

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Program {
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getValue().compareTo(p2.getValue());
+    }
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort(Program::compareProducts);
+        list.forEach(System.out::println);
+    }
+}
